e39cf2ee23cdf01e3f7e6f15b106607d2bc32987
NiccoloMorabito <morabito.niccolo@gmail.com>
2023-01-22 19:06:34 +0100

    Code to convert graphs in tensors for the pytorch pipeline

--- src/common_utils/process.py
+++ src/common_utils/process.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import scipy.sparse as sp
+import numpy as np
+import torch
 from common_utils.stg_utils import CATEGORIES
 
 pd.set_option('display.max_columns', None) 
@@ -15,7 +18,7 @@ def graph_to_feature_vector(graph):
     df = pd.DataFrame.from_dict(graph_to_dict(graph)).set_index("id")
     df["area"] = (df["x2"] - df["x1"]) * (df["y2"] - df["y1"]) #TODO decide if you want to keep the area
 
-    class_dummies = dummy_categories(df["class_name"])
+    class_dummies = dummy_categories(df["class_name"]) #TODO reduce the number of classes
     df = pd.concat([df,class_dummies], axis=1)
     df.drop('class_name', axis=1, inplace=True)
 
@@ -23,4 +26,19 @@ def graph_to_feature_vector(graph):
     df.drop('centroid', axis=1, inplace=True) #TODO check if it's important, but it's a tuple
     df.drop('detclass', axis=1, inplace=True) #TODO what is the meaning????
     #TODO you probably need to normalize (especially the coordinates and the area)
-    return df.values
+    return torch.FloatTensor(df.values[np.newaxis]) #TODO the new axis should be for the batch (?)
+
+def normalize_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+
+def adj_to_normalized_tensor(adj):
+    adj = normalize_adj(adj)
+    adj = (adj + sp.eye(adj.shape[0])).todense()
+    adj = torch.FloatTensor(adj[np.newaxis]) #TODO the new axis should be for the batch (?)
+    return adj
