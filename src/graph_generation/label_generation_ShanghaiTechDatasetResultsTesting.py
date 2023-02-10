0fa6157e9f683dd56d228896ba59c9f978908378
jaswu51 <506346013@qq.com>
2023-02-10 23:29:34 +0100

    push

--- src/graph_generation/label_generation_ShanghaiTechDatasetResultsTesting.py
+++ src/graph_generation/label_generation_ShanghaiTechDatasetResultsTesting.py
@@ -11,30 +11,30 @@ from scipy.spatial import distance
 import pickle
 
 
-list_key=[]
-list_value=[]
+# list_key=[]
+# list_value=[]
 
-for file in glob.glob(r"/Users/xiaokeai/Documents/GitHub/yolov7-main/datasets/shanghaitech/testing/test_frame_mask/*.npy"):
-    try:
-        a=numpy.load(file,allow_pickle=True)
-        # print(len(a))
-        # print(str(file))
-        list_key.append(str(file))
-        list_value.append(a)
-    except:
-        pass 
+# for file in glob.glob(r"/Users/xiaokeai/Documents/GitHub/yolov7-main/datasets/shanghaitech/testing/test_frame_mask/*.npy"):
+#     try:
+#         a=numpy.load(file,allow_pickle=True)
+#         # print(len(a))
+#         # print(str(file))
+#         list_key.append(str(file))
+#         list_value.append(a)
+#     except:
+#         pass 
 
-list_key_replace = [s.replace('/Users/xiaokeai/Documents/GitHub/yolov7-main/datasets/shanghaitech/testing/test_frame_mask/', '/Users/xiaokeai/Documents/GitHub/yolov7-main/datasets/shanghaitech/testing/frames/') for s in list_key]
-list_key_replace = [s.replace('.npy', '') for s in list_key_replace]
+# list_key_replace = [s.replace('/Users/xiaokeai/Documents/GitHub/yolov7-main/datasets/shanghaitech/testing/test_frame_mask/', '/Users/xiaokeai/Documents/GitHub/yolov7-main/datasets/shanghaitech/testing/frames/') for s in list_key]
+# list_key_replace = [s.replace('.npy', '') for s in list_key_replace]
 
-dic_label={}
-for i in range(len(list_key_replace)):
-    dic_label[list_key_replace[i]]=list(list_value[i])
+# dic_label={}
+# for i in range(len(list_key_replace)):
+#     dic_label[list_key_replace[i]]=list(list_value[i])
 
-with open('results/ShanghaiTech_testing_labels.pickle', 'wb') as handle:
-    pickle.dump(dic_label, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# with open('results/ShanghaiTech_testing_labels.pickle', 'wb') as handle:
+#     pickle.dump(dic_label, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-with open('results/ShanghaiTech_testing_labels.pickle', 'rb') as handle:
+with open('data/testing_graphs/ShanghaiTech_testing.pickle', 'rb') as handle:
      dictAvenue = pickle.load(handle)
      print(dictAvenue.keys())
 
